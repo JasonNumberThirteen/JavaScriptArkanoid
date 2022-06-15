419e9c6cc6b6092c1422bc395a0c2f5b24087c91
Jason <numberthirteen4026@gmail.com>
2022-06-15 22:21:49 +0200

    Added background color to game window

--- game.js
+++ game.js
@@ -2,4 +2,7 @@ function initGame()
 {
 	const canvas = document.getElementById("gameWindow");
 	const context = canvas.getContext("2d");
+
+	context.fillStyle = "#cccccc";
+	context.fillRect(0, 0, 256, 144);
 }
