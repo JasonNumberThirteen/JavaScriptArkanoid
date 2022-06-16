bbfdca2e5e60ef2d31147f0e5cbfb1f3d76f9827
Jason <numberthirteen4026@gmail.com>
2022-06-16 13:48:03 +0200

    Implementation of game loop

--- js/game.js
+++ js/game.js
@@ -11,16 +11,41 @@ class Game
 	#width;
 	#height;
 
+	#squareX = 0;
+	#squareSize = 16;
+
 	constructor()
 	{
 		const canvas = document.getElementById("gameWindow");
 		const cbcr = canvas.getBoundingClientRect();
 
 		this.#context = canvas.getContext("2d");
-		this.#context.fillStyle = "#cccccc";
 		this.#width = cbcr.width;
 		this.#height = cbcr.height;
 
+		window.requestAnimationFrame(this.loop.bind(this));
+	}
+
+	loop(timeStamp)
+	{
+		this.#draw();
+		
+		if(this.#squareX < this.#width - this.#squareSize)
+		{
+			window.requestAnimationFrame(this.loop.bind(this));
+		}
+	}
+
+	#draw()
+	{
+		this.#context.clearRect(0, 0, this.#width, this.#height);
+
+		this.#context.fillStyle = "#cccccc";
+
 		this.#context.fillRect(0, 0, this.#width, this.#height);
+
+		this.#context.fillStyle = "red";
+
+		this.#context.fillRect(this.#squareX++, 0, this.#squareSize, this.#squareSize);
 	}
 }
