aa5ce8c4d2952862cd98bc3b06e7af4f6574c035
Jason <numberthirteen4026@gmail.com>
2022-06-16 15:11:27 +0200

    Added Ball class

---
+++ js/ball.js
@@ -0,0 +1,28 @@
+class Ball
+{
+	#x;
+	#y;
+	#radius;
+	
+	constructor(radius, gameWidth, gameHeight)
+	{
+		this.#x = gameWidth >> 1;
+		this.#y = gameHeight >> 1;
+		this.#radius = radius;
+	}
+
+	draw(context)
+	{
+		context.fillStyle = "#42f2f2";
+
+		context.beginPath();
+		context.arc(this.#x, this.#y, this.#radius, 0, Math.PI*2);
+		context.fill();
+
+		context.fillStyle = "#000";
+
+		context.beginPath();
+		context.arc(this.#x, this.#y, this.#radius, 0, Math.PI*2);
+		context.stroke();
+	}
+}
