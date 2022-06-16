72fe0f77e3be5bd61d2d899b0bf30ec65463f949
Jason <numberthirteen4026@gmail.com>
2022-06-16 13:05:59 +0200

    Added width and height properties to Game class to match the canvas size

--- game.js
+++ game.js
@@ -8,14 +8,19 @@ function initGame()
 class Game
 {
 	#context;
+	#width;
+	#height;
 
 	constructor()
 	{
 		const canvas = document.getElementById("gameWindow");
+		const cbcr = canvas.getBoundingClientRect();
 
 		this.#context = canvas.getContext("2d");
 		this.#context.fillStyle = "#cccccc";
+		this.#width = cbcr.width;
+		this.#height = cbcr.height;
 
-		this.#context.fillRect(0, 0, 256, 144);
+		this.#context.fillRect(0, 0, this.#width, this.#height);
 	}
 }
