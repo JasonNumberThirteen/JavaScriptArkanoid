e550fed35472f96d1cd11ade5b4547e89aefc688
Jason <numberthirteen4026@gmail.com>
2022-06-17 19:42:00 +0200

    Optimized Math.PI expression in Ball class

--- js/ball.js
+++ js/ball.js
@@ -13,17 +13,19 @@ class Ball
 
 	draw(context)
 	{
+		const doubledPI = Math.PI << 1;
+		
 		context.fillStyle = "#42f2f2";
 
 		context.beginPath();
-		context.arc(this.#x, this.#y, this.#radius, 0, Math.PI*2);
+		context.arc(this.#x, this.#y, this.#radius, 0, doubledPI);
 		context.closePath();
 		context.fill();
 
 		context.fillStyle = "#000";
 
 		context.beginPath();
-		context.arc(this.#x, this.#y, this.#radius, 0, Math.PI*2);
+		context.arc(this.#x, this.#y, this.#radius, 0, doubledPI);
 		context.closePath();
 		context.stroke();
 	}
