138e73c6182adc75af9ed4cdc8066e36cf089c2b
Jason <numberthirteen4026@gmail.com>
2022-06-26 16:55:00 +0200

    Adjusted expressions in createBricks method

--- js/game.js
+++ js/game.js
@@ -52,11 +52,13 @@ class Game
 			for (let x = 1; x <= GAME_BRICKS_IN_ROW; ++x)
 			{
 				const values = GAME_BRICKS[y];
-				const brickX = GAME_BRICK_WIDTH + GAME_BRICK_GAP_X;
-				const brickY = GAME_BRICK_HEIGHT + GAME_BRICK_GAP_Y;
+				const baseX = GAME_BRICK_WIDTH + GAME_BRICK_GAP_X;
+				const baseY = GAME_BRICK_HEIGHT + GAME_BRICK_GAP_Y;
 				const offsetX = 15*GAME_WINDOW_SCALE;
 				const offsetY = GAME_HUD_HEIGHT + GAME_BRICK_GAP_Y;
-				const position = new Point(brickX*x - offsetX, brickY*y + offsetY);
+				const brickX = baseX*x - offsetX;
+				const brickY = baseY*y + offsetY;
+				const position = new Point(brickX, brickY);
 				const brick = new Brick(position, values);
 				
 				this.#bricks.push(brick);
