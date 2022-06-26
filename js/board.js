a4238ed486eb5692f8418a01b03af49e2db4d9cc
Jason <numberthirteen4026@gmail.com>
2022-06-26 18:41:54 +0200

    Moved code about objects and interactions to Board class

--- js/board.js
+++ js/board.js
@@ -1,4 +1,118 @@
 class Board
 {
+	#game;
+	#context;
+	#paddle;
+	#ball;
+	#bricks;
 
+	constructor(game, context)
+	{
+		this.#game = game;
+		this.#context = context;
+
+		this.#createObjects();
+	}
+
+	update(timeStamp)
+	{
+		this.#paddle.update();
+		this.#ball.update(timeStamp);
+		this.#checkCollisions();
+	}
+
+	draw()
+	{
+		this.#bricks.forEach(e => e.draw(this.#context));
+
+		if(this.#game.isStillRunning())
+		{
+			this.#paddle.draw(this.#context);
+			this.#ball.draw(this.#context);
+		}
+	}
+
+	getPaddle()
+	{
+		return this.#paddle;
+	}
+
+	destroyedAllBricks()
+	{
+		return this.#bricks.length === 0;
+	}
+
+	#createObjects()
+	{
+		this.#paddle = new Paddle(this.#game.getSize());
+		this.#ball = new Ball(GAME_BALL_RADIUS, this.#game.getSize());
+
+		this.#createBricks();
+	}
+
+	#createBricks()
+	{
+		this.#bricks = [];
+		
+		for (let y = 0; y <= GAME_BRICKS.length - 1; ++y)
+		{
+			for (let x = 1; x <= GAME_BRICKS_IN_ROW; ++x)
+			{
+				const values = GAME_BRICKS[y];
+				const baseX = GAME_BRICK_WIDTH + GAME_BRICK_GAP_X;
+				const baseY = GAME_BRICK_HEIGHT + GAME_BRICK_GAP_Y;
+				const offsetX = 15*GAME_WINDOW_SCALE;
+				const offsetY = GAME_HUD_HEIGHT + GAME_BRICK_GAP_Y;
+				const brickX = baseX*x - offsetX;
+				const brickY = baseY*y + offsetY;
+				const position = new Point(brickX, brickY);
+				const brick = new Brick(position, values);
+				
+				this.#bricks.push(brick);
+			}
+		}
+	}
+
+	#checkCollisions()
+	{
+		this.#checkCollisionBetweenPaddleAndBall();
+		this.#bricks.forEach(e => this.#checkCollisionBetweenBrickAndBall(e));
+
+		this.#bricks = this.#bricks.filter(e => e.isAlive());
+	}
+
+	#checkCollisionBetweenBrickAndBall(brick)
+	{
+		if(this.#rectangularObjectCollidesWithBall(brick, new Point(GAME_BRICK_WIDTH, GAME_BRICK_HEIGHT)))
+		{
+			this.#ball.deflect();
+			brick.takeDamage();
+
+			if(!brick.isAlive())
+			{
+				this.#game.addScore(brick.getPoints());
+			}
+		}
+	}
+
+	#checkCollisionBetweenPaddleAndBall()
+	{
+		if(this.#rectangularObjectCollidesWithBall(this.#paddle, new Point(GAME_PADDLE_WIDTH, GAME_PADDLE_HEIGHT)))
+		{
+			this.#ball.deflectFromPaddle();
+		}
+	}
+
+	#rectangularObjectCollidesWithBall(ro, size)
+	{
+		const rectangularObjectPosition = ro.getPosition();
+		const ballPosition = this.#ball.getPosition();
+		const rectangularObjectCollisionBox = new Point(rectangularObjectPosition.x + size.x, rectangularObjectPosition.y + size.y);
+		const xn = Math.max(rectangularObjectPosition.x, Math.min(ballPosition.x, rectangularObjectCollisionBox.x));
+		const yn = Math.max(rectangularObjectPosition.y, Math.min(ballPosition.y, rectangularObjectCollisionBox.y));
+		const dx = xn - ballPosition.x;
+		const dy = yn - ballPosition.y;
+
+		return dx*dx + dy*dy <= GAME_BALL_RADIUS*GAME_BALL_RADIUS;
+	}
 }
--- js/game.js
+++ js/game.js
@@ -9,10 +9,8 @@ class Game
 {
 	#context;
 	#size;
-	#paddle;
-	#ball;
-	#bricks = [];
 	#score = 0;
+	#board;
 	#ui;
 
 	constructor()
@@ -27,45 +25,16 @@ class Game
 		this.#context.font = GAME_FONT_SIZE + GAME_FONT_UNIT + " " + GAME_FONT;
 		this.#context.lineWidth = GAME_WINDOW_SCALE;
 		this.#size = new Point(width, height);
+		this.#board = new Board(this, this.#context);
 		this.#ui = new UI(this, this.#context);
 		
-		this.#createObjects();
 		this.#requestAnimationFrame();
 		document.addEventListener("keydown", this.#onKeyDown.bind(this), false);
 	}
 
 	onBallFall()
 	{
-		this.#paddle.loseLife();
-	}
-
-	#createObjects()
-	{
-		this.#paddle = new Paddle(this.#size);
-		this.#ball = new Ball(GAME_BALL_RADIUS, this.#size);
-
-		this.#createBricks();
-	}
-
-	#createBricks()
-	{
-		for (let y = 0; y <= GAME_BRICKS.length - 1; ++y)
-		{
-			for (let x = 1; x <= GAME_BRICKS_IN_ROW; ++x)
-			{
-				const values = GAME_BRICKS[y];
-				const baseX = GAME_BRICK_WIDTH + GAME_BRICK_GAP_X;
-				const baseY = GAME_BRICK_HEIGHT + GAME_BRICK_GAP_Y;
-				const offsetX = 15*GAME_WINDOW_SCALE;
-				const offsetY = GAME_HUD_HEIGHT + GAME_BRICK_GAP_Y;
-				const brickX = baseX*x - offsetX;
-				const brickY = baseY*y + offsetY;
-				const position = new Point(brickX, brickY);
-				const brick = new Brick(position, values);
-				
-				this.#bricks.push(brick);
-			}
-		}
+		this.#board.getPaddle().loseLife();
 	}
 
 	#loop(timeStamp)
@@ -86,31 +55,12 @@ class Game
 
 	#update(timeStamp)
 	{
-		this.#paddle.update();
-		this.#ball.update(timeStamp);
-		this.#checkCollisions();
+		this.#board.update(timeStamp);
 	}
 
-	#checkCollisions()
+	addScore(points)
 	{
-		this.#checkCollisionBetweenPaddleAndBall();
-		this.#bricks.forEach(e => this.#checkCollisionBetweenBrickAndBall(e));
-
-		this.#bricks = this.#bricks.filter(e => e.isAlive());
-	}
-
-	#checkCollisionBetweenBrickAndBall(brick)
-	{
-		if(this.#rectangularObjectCollidesWithBall(brick, new Point(GAME_BRICK_WIDTH, GAME_BRICK_HEIGHT)))
-		{
-			this.#ball.deflect();
-			brick.takeDamage();
-
-			if(!brick.isAlive())
-			{
-				this.#score += brick.getPoints();
-			}
-		}
+		this.#score += points;
 	}
 
 	isStillRunning()
@@ -130,43 +80,17 @@ class Game
 
 	getPaddleLives()
 	{
-		return this.#paddle.getLives();
+		return this.#board.getPaddle().getLives();
 	}
 
 	wonTheGame()
 	{
-		return this.#destroyedAllBricks();
+		return this.#board.destroyedAllBricks();
 	}
 
 	#lostTheGame()
 	{
-		return this.#paddle.lostAllLives();
-	}
-
-	#destroyedAllBricks()
-	{
-		return this.#bricks.length === 0;
-	}
-
-	#checkCollisionBetweenPaddleAndBall()
-	{
-		if(this.#rectangularObjectCollidesWithBall(this.#paddle, new Point(GAME_PADDLE_WIDTH, GAME_PADDLE_HEIGHT)))
-		{
-			this.#ball.deflectFromPaddle();
-		}
-	}
-
-	#rectangularObjectCollidesWithBall(ro, size)
-	{
-		const rectangularObjectPosition = ro.getPosition();
-		const ballPosition = this.#ball.getPosition();
-		const rectangularObjectCollisionBox = new Point(rectangularObjectPosition.x + size.x, rectangularObjectPosition.y + size.y);
-		const xn = Math.max(rectangularObjectPosition.x, Math.min(ballPosition.x, rectangularObjectCollisionBox.x));
-		const yn = Math.max(rectangularObjectPosition.y, Math.min(ballPosition.y, rectangularObjectCollisionBox.y));
-		const dx = xn - ballPosition.x;
-		const dy = yn - ballPosition.y;
-
-		return dx*dx + dy*dy <= GAME_BALL_RADIUS*GAME_BALL_RADIUS;
+		return this.#board.getPaddle().lostAllLives();
 	}
 
 	#draw()
@@ -174,14 +98,7 @@ class Game
 		this.#context.clearRect(0, 0, this.#size.x, this.#size.y);
 		this.#drawBG(0, GAME_HUD_HEIGHT, GAME_HUD_FILL_STYLE);
 		this.#drawBG(GAME_HUD_HEIGHT, this.#size.y - GAME_HUD_HEIGHT, GAME_BG_FILL_STYLE);
-		this.#bricks.forEach(e => e.draw(this.#context));
-
-		if(this.isStillRunning())
-		{
-			this.#paddle.draw(this.#context);
-			this.#ball.draw(this.#context);
-		}
-
+		this.#board.draw();
 		this.#ui.draw(this.#context);
 	}
 
