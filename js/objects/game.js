a74b768988709931b1eeb1da77b1e04f28c59044
Jason <numberthirteen4026@gmail.com>
2022-07-01 14:09:14 +0200

    Organized methods order in Game class

--- js/objects/game.js
+++ js/objects/game.js
@@ -23,6 +23,27 @@ class Game
 	{
 		return !(this.#gameManager.wonTheGame() || this.#gameManager.lostTheGame());
 	}
+
+	#init()
+	{
+		this.#context = document.getElementById("gameWindow").getContext("2d");
+		this.#board = new Board(this);
+		this.#gameManager = new GameManager(this.#board);
+		this.#renderer = new Renderer(this.#board);
+		this.#ui = new UI(this, this.#context);
+	}
+
+	#setCanvasSize()
+	{
+		this.#context.canvas.width = GAME_WIDTH;
+		this.#context.canvas.height = GAME_HEIGHT;
+	}
+
+	#setContextValues()
+	{
+		this.#context.font = GAME_FONT_SIZE + GAME_FONT_UNIT + " " + GAME_FONT;
+		this.#context.lineWidth = GAME_WINDOW_SCALE;
+	}
 	
 	#requestAnimationFrame()
 	{
@@ -50,26 +71,5 @@ class Game
 		this.#context.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
 		this.#renderer.draw(this.#context);
 		this.#ui.draw();
-	}
-
-	#init()
-	{
-		this.#context = document.getElementById("gameWindow").getContext("2d");
-		this.#board = new Board(this);
-		this.#gameManager = new GameManager(this.#board);
-		this.#renderer = new Renderer(this.#board);
-		this.#ui = new UI(this, this.#context);
-	}
-
-	#setCanvasSize()
-	{
-		this.#context.canvas.width = GAME_WIDTH;
-		this.#context.canvas.height = GAME_HEIGHT;
-	}
-
-	#setContextValues()
-	{
-		this.#context.font = GAME_FONT_SIZE + GAME_FONT_UNIT + " " + GAME_FONT;
-		this.#context.lineWidth = GAME_WINDOW_SCALE;
 	}
 }
