fe6a18fb1ce88d920dfc8661fc8b941391143f90
Jason <numberthirteen4026@gmail.com>
2022-07-02 20:37:33 +0200

    Moved code about canvas context to CanvasContext class

--- js/objects/canvasContext.js
+++ js/objects/canvasContext.js
@@ -1,4 +1,35 @@
 class CanvasContext
 {
-	
+	#context;
+	#renderer;
+	#ui;
+
+	constructor(game, board)
+	{
+		this.#context = document.getElementById("gameWindow").getContext("2d");
+		this.#renderer = new Renderer(board);
+		this.#ui = new UI(game, this.#context);
+
+		this.#setCanvasSize();
+		this.#setContextValues();
+	}
+
+	draw()
+	{
+		this.#context.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+		this.#renderer.draw(this.#context);
+		this.#ui.draw();
+	}
+
+	#setCanvasSize()
+	{
+		this.#context.canvas.width = GAME_WIDTH;
+		this.#context.canvas.height = GAME_HEIGHT;
+	}
+
+	#setContextValues()
+	{
+		this.#context.font = GAME_FONT_SIZE + GAME_FONT_UNIT + " " + GAME_FONT;
+		this.#context.lineWidth = GAME_WINDOW_SCALE;
+	}
 }
--- js/objects/game.js
+++ js/objects/game.js
@@ -3,14 +3,10 @@ class Game
 	#context;
 	#board;
 	#gameManager;
-	#renderer;
-	#ui;
 
 	constructor()
 	{
 		this.#init();
-		this.#setCanvasSize();
-		this.#setContextValues();
 		this.#requestAnimationFrame();
 	}
 
@@ -26,23 +22,9 @@ class Game
 
 	#init()
 	{
-		this.#context = document.getElementById("gameWindow").getContext("2d");
 		this.#board = new Board(this);
 		this.#gameManager = new GameManager(this.#board);
-		this.#renderer = new Renderer(this.#board);
-		this.#ui = new UI(this, this.#context);
-	}
-
-	#setCanvasSize()
-	{
-		this.#context.canvas.width = GAME_WIDTH;
-		this.#context.canvas.height = GAME_HEIGHT;
-	}
-
-	#setContextValues()
-	{
-		this.#context.font = GAME_FONT_SIZE + GAME_FONT_UNIT + " " + GAME_FONT;
-		this.#context.lineWidth = GAME_WINDOW_SCALE;
+		this.#context = new CanvasContext(this, this.#board);
 	}
 	
 	#requestAnimationFrame()
@@ -53,7 +35,7 @@ class Game
 	#loop(timeStamp)
 	{
 		this.#update(timeStamp);
-		this.#draw();
+		this.#context.draw();
 
 		if(this.isRunning())
 		{
@@ -64,12 +46,5 @@ class Game
 	#update(timeStamp)
 	{
 		this.#board.update(timeStamp);
-	}
-
-	#draw()
-	{
-		this.#context.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-		this.#renderer.draw(this.#context);
-		this.#ui.draw();
 	}
 }
