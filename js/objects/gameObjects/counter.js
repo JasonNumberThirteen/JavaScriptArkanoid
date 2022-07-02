3fbdaa75a85f491813d0cbd55aafef80b9aa2148
Jason <numberthirteen4026@gmail.com>
2022-07-02 21:24:22 +0200

    Implemented Counter class

--- js/objects/gameObjects/counter.js
+++ js/objects/gameObjects/counter.js
@@ -1,4 +1,24 @@
 class Counter
 {
-	
+	#value;
+
+	constructor(value)
+	{
+		this.#value = value;
+	}
+
+	increaseBy(n)
+	{
+		this.#value += n;
+	}
+
+	decreaseBy(n)
+	{
+		this.#value -= n;
+	}
+
+	getValue()
+	{
+		return this.#value;
+	}
 }
