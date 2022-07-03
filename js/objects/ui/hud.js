dad700f2fba241f881f9ac2f4c2149c1fdf6c904
Jason <numberthirteen4026@gmail.com>
2022-07-03 14:26:53 +0200

    Added a calculation of offset from HUD texts to counters

--- js/objects/ui/hud.js
+++ js/objects/ui/hud.js
@@ -15,8 +15,10 @@ class HUD
 
 	#drawScoreCounter(ui, context)
 	{
+		const metrics = context.measureText(GAME_SCORE_TEXT);
+		const counterOffsetFromText = metrics.actualBoundingBoxAscent;
 		const textPosition = new Point(GAME_HUD_TEXTS_OFFSET_X, GAME_HUD_TEXTS_Y);
-		const counterPosition = new Point(textPosition.x + GAME_HUD_COUNTERS_OFFSET, textPosition.y + GAME_HUD_COUNTERS_OFFSET);
+		const counterPosition = new Point(textPosition.x + GAME_HUD_COUNTERS_OFFSET, textPosition.y + counterOffsetFromText);
 		const align = "left";
 		
 		ui.drawText(context, GAME_SCORE_TEXT, textPosition, GAME_HUD_TEXTS_FILL_STYLE, align);
@@ -25,8 +27,10 @@ class HUD
 
 	#drawLivesCounter(ui, context)
 	{
+		const metrics = context.measureText(GAME_LIVES_TEXT);
+		const counterOffsetFromText = metrics.actualBoundingBoxAscent;
 		const textPosition = new Point(GAME_WIDTH - GAME_HUD_TEXTS_OFFSET_X, GAME_HUD_TEXTS_Y);
-		const counterPosition = new Point(textPosition.x - GAME_HUD_COUNTERS_OFFSET, textPosition.y + GAME_HUD_COUNTERS_OFFSET);
+		const counterPosition = new Point(textPosition.x - GAME_HUD_COUNTERS_OFFSET, textPosition.y + counterOffsetFromText);
 		const align = "right";
 		
 		ui.drawText(context, GAME_LIVES_TEXT, textPosition, GAME_HUD_TEXTS_FILL_STYLE, align);
