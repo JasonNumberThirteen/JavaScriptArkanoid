943927ede07dbeb7df005ef923a9a9ac880ad92f
Jason <numberthirteen4026@gmail.com>
2022-07-03 14:42:51 +0200

    Added a calculation of offset to the HUD counters to place them exactly one letter forward from texts

--- js/objects/ui/hud.js
+++ js/objects/ui/hud.js
@@ -15,10 +15,11 @@ class HUD
 
 	#drawScoreCounter(ui, context)
 	{
-		const metrics = context.measureText(GAME_SCORE_TEXT);
-		const counterOffsetFromText = metrics.actualBoundingBoxAscent + GAME_WINDOW_SCALE;
+		const textMetrics = context.measureText(GAME_SCORE_TEXT);
+		const textFirstLetterMetrics = context.measureText(GAME_SCORE_TEXT.slice(0, 1));
+		const counterOffsetFromText = textMetrics.actualBoundingBoxAscent + GAME_WINDOW_SCALE;
 		const textPosition = new Point(GAME_HUD_TEXTS_OFFSET_X, GAME_HUD_TEXTS_Y);
-		const counterPosition = new Point(textPosition.x + GAME_HUD_COUNTERS_OFFSET, textPosition.y + counterOffsetFromText);
+		const counterPosition = new Point(textPosition.x + textFirstLetterMetrics.width, textPosition.y + counterOffsetFromText);
 		const align = "left";
 		
 		ui.drawText(context, GAME_SCORE_TEXT, textPosition, GAME_HUD_TEXTS_FILL_STYLE, align);
@@ -27,10 +28,11 @@ class HUD
 
 	#drawLivesCounter(ui, context)
 	{
-		const metrics = context.measureText(GAME_LIVES_TEXT);
-		const counterOffsetFromText = metrics.actualBoundingBoxAscent + GAME_WINDOW_SCALE;
+		const textMetrics = context.measureText(GAME_LIVES_TEXT);
+		const textLastLetterMetrics = context.measureText(GAME_LIVES_TEXT.slice(GAME_LIVES_TEXT.length - 2, GAME_LIVES_TEXT.length - 1));
+		const counterOffsetFromText = textMetrics.actualBoundingBoxAscent + GAME_WINDOW_SCALE;
 		const textPosition = new Point(GAME_WIDTH - GAME_HUD_TEXTS_OFFSET_X, GAME_HUD_TEXTS_Y);
-		const counterPosition = new Point(textPosition.x - GAME_HUD_COUNTERS_OFFSET, textPosition.y + counterOffsetFromText);
+		const counterPosition = new Point(textPosition.x - textLastLetterMetrics.width, textPosition.y + counterOffsetFromText);
 		const align = "right";
 		
 		ui.drawText(context, GAME_LIVES_TEXT, textPosition, GAME_HUD_TEXTS_FILL_STYLE, align);
