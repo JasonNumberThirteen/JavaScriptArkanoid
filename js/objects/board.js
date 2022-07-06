320e899a8a6dd0068fa8599035e88aa7303c495f
Jason <numberthirteen4026@gmail.com>
2022-07-06 15:44:22 +0200

    Moved code about creating bricks to BrickFactory class

--- js/objects/board.js
+++ js/objects/board.js
@@ -41,33 +41,13 @@ class Board
 	{
 		this.#paddle = new Paddle();
 		this.#ball = new Ball();
-		this.#bricks = [];
 	}
 
 	#createBricks()
 	{
-		for (let y = 0; y <= GAME_BRICKS.length - 1; ++y)
-		{
-			for (let x = 1; x <= GAME_BRICKS_IN_ROW; ++x)
-			{
-				this.#createBrick(x, y);
-			}
-		}
-	}
+		const brickFactory = new BrickFactory();
 
-	#createBrick(column, row)
-	{
-		const brick = new Brick(this.#brickPosition(column, row), GAME_BRICKS[row]);
-		
-		this.#bricks.push(brick);
-	}
-
-	#brickPosition(column, row)
-	{
-		const basePosition = new Point(GAME_BRICK_WIDTH + GAME_BRICK_GAP_X, GAME_BRICK_HEIGHT + GAME_BRICK_GAP_Y);
-		const offset = new Point(15*GAME_WINDOW_SCALE, GAME_HUD_HEIGHT + GAME_BRICK_GAP_Y);
-		
-		return new Point(basePosition.x*column - offset.x, basePosition.y*row + offset.y);
+		this.#bricks = brickFactory.bricksTable();
 	}
 
 	#checkCollisions()
--- js/objects/brickFactory.js
+++ js/objects/brickFactory.js
@@ -1,4 +1,30 @@
 class BrickFactory
 {
+	bricksTable()
+	{
+		const bricks = [];
+		
+		for (let y = 0; y <= GAME_BRICKS.length - 1; ++y)
+		{
+			for (let x = 1; x <= GAME_BRICKS_IN_ROW; ++x)
+			{
+				bricks.push(this.#brickInstance(x, y));
+			}
+		}
 
+		return bricks;
+	}
+
+	#brickInstance(column, row)
+	{
+		return new Brick(this.#brickPosition(column, row), GAME_BRICKS[row]);
+	}
+
+	#brickPosition(column, row)
+	{
+		const basePosition = new Point(GAME_BRICK_WIDTH + GAME_BRICK_GAP_X, GAME_BRICK_HEIGHT + GAME_BRICK_GAP_Y);
+		const offset = new Point(15*GAME_WINDOW_SCALE, GAME_HUD_HEIGHT + GAME_BRICK_GAP_Y);
+		
+		return new Point(basePosition.x*column - offset.x, basePosition.y*row + offset.y);
+	}
 }
